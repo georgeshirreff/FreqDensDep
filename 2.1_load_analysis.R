dda75053aeb5bd9c84c20e3ef22ce40f3a36ed51
georgeshirreff <georgeshirreff@gmail.com>
2024-10-29 15:13:00 +0100

    Modifications of figures for review

--- 2.1_load_analysis.R
+++ 2.1_load_analysis.R
@@ -8,13 +8,22 @@ ward_intervals_catHosp <- read_csv("data/FreqDens_ward_intervals_catHosp.csv"
                                , locale = locale(tz = "CET"))
 
 
-typeID <- read_csv("data/typeID.csv")
+# typeID <- read_csv("data/typeID.csv")
+typeID <- read_csv("data/typeID_hospAbb.csv")
 
 newID_ref <- typeID %>% 
-  select(ward_id, newID) %>% 
-  arrange(newID) %>% 
-  rbind(tibble(ward_id = 0, newID = "All wards")) %>% 
+  arrange(newID) %>%
+  mutate(newID_hospAbb = paste0(hospAbb, " - ", newID)) %>% 
+  select(ward_id, newID, newID_hospAbb) %>% 
+  rbind(tibble(ward_id = 0, newID = "All wards", newID_hospAbb = "All wards")) %>% 
   mutate(newID = factor(newID, levels = newID))
+
+
+# newID_ref <- typeID %>% 
+#   select(ward_id, newID) %>% 
+#   arrange(newID) %>% 
+#   rbind(tibble(ward_id = 0, newID = "All wards")) %>% 
+#   mutate(newID = factor(newID, levels = newID))
 
 
 # read results of Bayesian analysis
@@ -61,9 +70,11 @@ ward_intervals_loglik <- ward_intervals_edit %>%
   {rbind(., mutate(., ward_id = 0))} %>% 
   left_join(newID_ref) %>% 
   filter(!is.na(newID)) %>% 
-  left_join(ward_nonlinear_main %>% filter(Period == "All") %>% transmute(ward_id, this_status, that_status, phi_nondiurnal = phi_median)) %>%
-  left_join(ward_nonlinear_main %>% filter(Period != "All") %>% transmute(ward_id, Period, this_status, that_status, phi_diurnal = phi_median)) %>%
-  mutate(phi_diurnal = ifelse(is.na(phi_diurnal), phi_nondiurnal, phi_diurnal)) %>% 
+  left_join(ward_nonlinear_main %>% filter(Period == "All") %>% transmute(ward_id, this_status, that_status, phi_nondiurnal = phi_median, phi_nondiurnal_lo = phi_lo, phi_nondiurnal_hi = phi_hi)) %>%
+  left_join(ward_nonlinear_main %>% filter(Period != "All") %>% transmute(ward_id, Period, this_status, that_status, phi_diurnal = phi_median, phi_diurnal_lo = phi_lo, phi_diurnal_hi = phi_hi)) %>%
+  mutate(phi_diurnal = ifelse(is.na(phi_diurnal), phi_nondiurnal, phi_diurnal), 
+         phi_diurnal_lo = ifelse(is.na(phi_diurnal_lo), phi_nondiurnal_lo, phi_diurnal_lo), 
+         phi_diurnal_hi = ifelse(is.na(phi_diurnal_hi), phi_nondiurnal_hi, phi_diurnal_hi)) %>% 
   # group_by(ward_id, this_status, that_status) %>% 
   # filter(form_minutes > 0) %>% 
   # mutate(var = var(contactIntensity)) %>% 
@@ -71,18 +82,34 @@ ward_intervals_loglik <- ward_intervals_edit %>%
   group_by(ward_id, this_status, that_status) %>%
   mutate(w_constant = sum(break_minutes)/sum(form_minutes)
          , w_linear = sum(break_minutes)/sum(form_minutes*Nthat)
-         , w_nonlin = sum(break_minutes)/sum(form_minutes*Nthat^phi_nondiurnal)) %>% 
+         , w_nonlin = sum(break_minutes)/sum(form_minutes*Nthat^phi_nondiurnal), 
+         , w_nonlin_lo = sum(break_minutes)/sum(form_minutes*Nthat^phi_nondiurnal_lo), 
+         , w_nonlin_hi = sum(break_minutes)/sum(form_minutes*Nthat^phi_nondiurnal_hi), 
+         , sd_constant = sd(break_minutes/form_minutes)
+         , sd_linear = sd(break_minutes/form_minutes*Nthat)
+         , sd_nonlin = sd(break_minutes/form_minutes*Nthat^phi_nondiurnal)
+         , sd_nonlin_lo = sd(break_minutes/form_minutes*Nthat^phi_nondiurnal_lo)
+         , sd_nonlin_hi = sd(break_minutes/form_minutes*Nthat^phi_nondiurnal_hi)
+         ) %>% 
   group_by(ward_id, this_status, that_status, Period) %>% #group by day/night to calculate the day and night parameters
   mutate(w_cdiurnal = sum(break_minutes)/sum(form_minutes)
          , w_ldiurnal = sum(break_minutes)/sum(form_minutes*Nthat)
-         , w_ndiurnal = sum(break_minutes)/sum(form_minutes*Nthat^phi_diurnal)) %>% 
+         , w_ndiurnal = sum(break_minutes)/sum(form_minutes*Nthat^phi_diurnal), 
+         , w_ndiurnal_lo = sum(break_minutes)/sum(form_minutes*Nthat^phi_diurnal_lo), 
+         , w_ndiurnal_hi = sum(break_minutes)/sum(form_minutes*Nthat^phi_diurnal_hi), 
+         , sd_cdiurnal = sd(break_minutes/form_minutes)
+         , sd_ldiurnal = sd(break_minutes/form_minutes*Nthat)
+         , sd_ndiurnal = sd(break_minutes/form_minutes*Nthat^phi_diurnal)
+         , sd_ndiurnal_lo = sd(break_minutes/form_minutes*Nthat^phi_diurnal_lo)
+         , sd_ndiurnal_hi = sd(break_minutes/form_minutes*Nthat^phi_diurnal_hi)
+         ) %>% 
   mutate(rate_constant = w_constant #calculate the rates for both non-diurnal and diurnal models
          , rate_linear = w_linear*Nthat
          , rate_nonlin = w_nonlin*Nthat^phi_nondiurnal
          , rate_cdiurnal = w_cdiurnal
          , rate_ldiurnal = w_ldiurnal*Nthat
          , rate_ndiurnal = w_ndiurnal*Nthat^phi_diurnal
-  ) %>%
+         ) %>%
   
   ## calculate corresponding likelihoods for each time window
   mutate(across(starts_with("rate")
@@ -90,6 +117,7 @@ ward_intervals_loglik <- ward_intervals_edit %>%
                 , .names = "{.fn}_{.col}")) %>% 
   rename_all(function(x) gsub("loglik_rate_", "loglik_", x))
 
+ward_intervals_loglik %>% names
 
 
 ### and for catHosp
--- 2.3_make_figures.R
+++ 2.3_make_figures.R
@@ -38,23 +38,61 @@ chosen_model_catHosp_allthisthat <- read_tsv(paste0("output/dAIC_catHosp_allthis
 
 ### PLOT GRAPHS SHOWING THE BEST MODEL SLOPES ###
 
+# slope_model_colours = c(`.data` = "black", 
+#                   `Non-linear (negative)` = alpha("darkblue", 0.6), 
+#                   `Non-linear (negative) - diurnal` = alpha("darkblue", 1), 
+#                   `Frequency dependence` = alpha("blue", 0.6), 
+#                   `Frequency dependence - diurnal` = alpha("blue", 1), 
+#                   `Non-linear (partial)` = alpha("green", 0.6), 
+#                   `Non-linear (partial) - diurnal` = alpha("green", 1), 
+#                   `Linear density dependence` = alpha("gold", 0.6), 
+#                   `Linear density dependence - diurnal` = alpha("gold", 1), 
+#                   `Non-linear (more than linear)` = alpha("orange", 0.6), 
+#                   `Non-linear (more than linear) - diurnal` = alpha("orange", 1))
+
+
 slope_model_colours = c(`.data` = "black", 
-                  `Non-linear (negative)` = alpha("darkblue", 0.6), 
-                  `Non-linear (negative) - diurnal` = alpha("darkblue", 1), 
-                  `Frequency dependence` = alpha("blue", 0.6), 
-                  `Frequency dependence - diurnal` = alpha("blue", 1), 
-                  `Non-linear (partial)` = alpha("green", 0.6), 
-                  `Non-linear (partial) - diurnal` = alpha("green", 1), 
-                  `Linear density dependence` = alpha("gold", 0.6), 
-                  `Linear density dependence - diurnal` = alpha("gold", 1), 
-                  `Non-linear (more than linear)` = alpha("orange", 0.6), 
-                  `Non-linear (more than linear) - diurnal` = alpha("orange", 1))
+                        `Non-linear (negative)` = "blue4", 
+                        `Frequency dependence` = "blue", 
+                        `Non-linear (partial)` = "green", 
+                        `Linear density dependence` = "gold", 
+                        `Non-linear (more than linear)` = "orange") 
 
 greek = "phi"
 
-toPlot_N_chosenModels_Slope <- ward_intervals_loglik %>%
+toPlot_N_chosenModels_Slope <- ward_intervals_loglik %>% 
   filter((this_status == "ALL" & that_status == "ALL") | 
            ((this_status %in% c("PA", "PE")) & (that_status == "PA"))) %>% 
+  ungroup %>% 
+  select(newID, this_status, that_status, Period, starts_with("phi"), starts_with("w_"), starts_with("sd_")) %>% unique %>% 
+  cross_join(tibble(Nthat = 1:100)) %>% 
+  mutate(rate_constant = w_constant #calculate the rates for both non-diurnal and diurnal models
+         , rate_linear = w_linear*Nthat
+         , rate_nonlin = w_nonlin*Nthat^phi_nondiurnal
+         , rate_cdiurnal = w_cdiurnal
+         , rate_ldiurnal = w_ldiurnal*Nthat
+         , rate_ndiurnal = w_ndiurnal*Nthat^phi_diurnal
+         
+         , rate_nonlin_lo = w_nonlin_lo*Nthat^phi_nondiurnal_lo
+         , rate_nonlin_hi = w_nonlin_hi*Nthat^phi_nondiurnal_hi
+         
+         , rate_ndiurnal_lo = w_ndiurnal_lo*Nthat^phi_diurnal_lo
+         , rate_ndiurnal_hi = w_ndiurnal_hi*Nthat^phi_diurnal_hi
+         
+         # , rate_constant_lo = w_constant - sd_constant #calculate the rates for both non-diurnal and diurnal models
+         # , rate_linear_lo = (w_linear - sd_linear)*Nthat
+         # , rate_nonlin_lo = (w_nonlin_lo - sd_nonlin_lo)*Nthat^phi_nondiurnal_lo
+         # , rate_cdiurnal_lo = w_cdiurnal - sd_cdiurnal
+         # , rate_ldiurnal_lo = (w_ldiurnal - sd_ldiurnal)*Nthat
+         # , rate_ndiurnal_lo = (w_ndiurnal_lo - sd_ndiurnal_lo)*Nthat^phi_diurnal_lo
+         # 
+         # , rate_constant_hi = w_constant + sd_constant #calculate the rates for both non-diurnal and diurnal models
+         # , rate_linear_hi = (w_linear + sd_linear)*Nthat
+         # , rate_nonlin_hi = (w_nonlin_hi + sd_nonlin_hi)*Nthat^phi_nondiurnal_hi
+         # , rate_cdiurnal_hi = w_cdiurnal + sd_cdiurnal
+         # , rate_ldiurnal_hi = (w_ldiurnal + sd_ldiurnal)*Nthat
+         # , rate_ndiurnal_hi = (w_ndiurnal_hi + sd_ndiurnal_hi)*Nthat^phi_diurnal_hi
+  ) %>% 
   # left_join(chosen_model) %>%
   left_join(rbind(chosen_model %>% mutate(this_status = "ALL", that_status = "ALL")
                   , chosen_model_thisthat)) %>%
@@ -64,8 +102,19 @@ toPlot_N_chosenModels_Slope <- ward_intervals_loglik %>%
                                       , ChosenModel == "Non-linear" ~ rate_nonlin
                                       , ChosenModel == "Constant diurnal" ~ rate_cdiurnal
                                       , ChosenModel == "Linear diurnal" ~ rate_ldiurnal
-                                      , ChosenModel == "Non-linear diurnal" ~ rate_ndiurnal
-  )) %>% 
+                                      , ChosenModel == "Non-linear diurnal" ~ rate_ndiurnal)) %>% 
+  mutate(rate_chosenmodel_lo = case_when(ChosenModel == "Constant" ~ rate_constant
+                                      , ChosenModel == "Linear" ~ rate_linear
+                                      , ChosenModel == "Non-linear" ~ rate_nonlin_lo
+                                      , ChosenModel == "Constant diurnal" ~ rate_cdiurnal
+                                      , ChosenModel == "Linear diurnal" ~ rate_ldiurnal
+                                      , ChosenModel == "Non-linear diurnal" ~ rate_ndiurnal_lo)) %>% 
+  mutate(rate_chosenmodel_hi = case_when(ChosenModel == "Constant" ~ rate_constant
+                                      , ChosenModel == "Linear" ~ rate_linear
+                                      , ChosenModel == "Non-linear" ~ rate_nonlin_hi
+                                      , ChosenModel == "Constant diurnal" ~ rate_cdiurnal
+                                      , ChosenModel == "Linear diurnal" ~ rate_ldiurnal
+                                      , ChosenModel == "Non-linear diurnal" ~ rate_ndiurnal_hi)) %>% 
   mutate(param_label = case_when(ChosenModel == "Non-linear" ~ paste0(greek, "==", round(phi_nondiurnal, 1))
                                  , ChosenModel == "Non-linear diurnal" ~ paste0(greek, "==", round(phi_diurnal, 1))
                                  , ChosenModel == "Constant" ~ paste0(greek, "==", 0)
@@ -73,39 +122,53 @@ toPlot_N_chosenModels_Slope <- ward_intervals_loglik %>%
                                  , ChosenModel == "Linear" ~ paste0(greek, "==", 1)
                                  , ChosenModel == "Linear diurnal" ~ paste0(greek, "==", 1)
                                  , T ~ "")) %>% 
-  mutate(model_label = case_when(ChosenModel == "Constant" ~ "Frequency dependence"
-                                 , ChosenModel == "Constant diurnal" ~ "Frequency dependence - diurnal"
-                                 , ChosenModel == "Linear" ~ "Linear density dependence"
-                                 , ChosenModel == "Linear diurnal" ~ "Linear density dependence - diurnal"
-                                 , ChosenModel == "Non-linear" & phi_nondiurnal < 0 ~ "Non-linear (negative)"
-                                 , ChosenModel == "Non-linear" & phi_nondiurnal < 1 ~ "Non-linear (partial)"
-                                 , ChosenModel == "Non-linear" ~ "Non-linear (more than linear)"
-                                 , ChosenModel == "Non-linear diurnal" & phi_diurnal < 0 ~ "Non-linear (negative) - diurnal"
-                                 , ChosenModel == "Non-linear diurnal" & phi_diurnal < 1 ~ "Non-linear (partial) - diurnal"
-                                 , ChosenModel == "Non-linear diurnal" ~ "Non-linear (more than linear) - diurnal"
+  mutate(chosen_model_slope = case_when(ChosenModel == "Constant" ~ "Frequency dependence"
+                                        , ChosenModel == "Constant diurnal" ~ "Frequency dependence - diurnal"
+                                        , ChosenModel == "Linear" ~ "Linear density dependence"
+                                        , ChosenModel == "Linear diurnal" ~ "Linear density dependence - diurnal"
+                                        , ChosenModel == "Non-linear" & phi_nondiurnal < 0 ~ "Non-linear (negative)"
+                                        , ChosenModel == "Non-linear" & phi_nondiurnal < 1 ~ "Non-linear (partial)"
+                                        , ChosenModel == "Non-linear" ~ "Non-linear (more than linear)"
+                                        , ChosenModel == "Non-linear diurnal" & phi_diurnal < 0 ~ "Non-linear (negative) - diurnal"
+                                        , ChosenModel == "Non-linear diurnal" & phi_diurnal < 1 ~ "Non-linear (partial) - diurnal"
+                                        , ChosenModel == "Non-linear diurnal" ~ "Non-linear (more than linear) - diurnal"
   )) %>% 
-  mutate(model_label = factor(model_label, levels = names(slope_model_colours)))
-
+  mutate(isDiurnal = ifelse(grepl("diurnal", ChosenModel), "Diurnal", "Non-diurnal"), 
+         model_label = gsub(" - diurnal", "", chosen_model_slope)) %>% 
+  mutate(model_label = factor(model_label, levels = names(slope_model_colours))) %>% 
+  select(newID, this_status, that_status, Period, Nthat, ChosenModel, model_label, isDiurnal, param_label, rate_chosenmodel, rate_chosenmodel_lo, rate_chosenmodel_hi) %>% 
+  filter(rate_chosenmodel <= 1)
 
 
 
 plot_N_chosenModels_Slope = toPlot_N_chosenModels_Slope %>% 
   filter(this_status == "ALL", that_status == "ALL") %>%
   ggplot(aes(x = Nthat)) + 
-  geom_point(aes(y = contactIntensity, colour = factor(".data", levels = names(slope_model_colours))), alpha = 0.3, size = 0.3) + 
+  geom_line(aes(y = rate_chosenmodel, colour = model_label, linetype = isDiurnal), linewidth = 0.5) +
+  geom_ribbon(aes(ymin = rate_chosenmodel_lo, ymax = rate_chosenmodel_hi, fill = model_label), alpha = 0.3, show.legend = F) +
   # geom_point(aes(x = mean_Nthat, y = obs_intensity, colour = ".data"), alpha = 1) +
   # geom_errorbar(aes(x = mean_Nthat, ymin = obs_intensity - sqrt(var_intensity), ymax = obs_intensity + sqrt(var_intensity), colour = ".data"), alpha = 1) +
-  geom_line(aes(y = rate_chosenmodel, colour = model_label), linewidth = 1.5, show.legend = T) +
-  geom_text(aes(x = Inf, y = 3, label = param_label), parse = T, hjust = 1.0, vjust = 1) +
+  # geom_line(aes(y = rate_chosenmodel, colour = model_label), linewidth = 1.0, show.legend = T) +
+  # geom_line(aes(y = rate_chosenmodel, colour = model_label, linetype = diurnality), linewidth = 1.0, show.legend = T) +
+  geom_point(data = ward_intervals_loglik %>% 
+               filter(this_status == "ALL", that_status == "ALL"), aes(y = contactIntensity, colour = factor(".data", levels = names(slope_model_colours))), alpha = 0.3, size = 0.3) + 
+  geom_text(data = toPlot_N_chosenModels_Slope %>% 
+              filter(this_status == "ALL", that_status == "ALL") %>% 
+              select(newID, Period, param_label) %>% unique, 
+            aes(x = Inf, y = 3, label = param_label), parse = T, hjust = 1.0, vjust = 1) +
   scale_colour_manual(drop = F, values = slope_model_colours) +
+  scale_fill_manual(values = slope_model_colours) +
+  scale_linetype_manual(values = c(Diurnal = "solid", `Non-diurnal` = "dashed")) + 
   # geom_crossbar(aes(x = mean_Nthat, xmin = low_Nthat, xmax = high_Nthat, y = mean_break_rate, colour = "break"), size=1) + 
   facet_nested_wrap(.~newID + Period
-                    , scales = "free_x"
+                    # , scales = "free_x"
                     ,  nrow = 2) +
   # guides(colour = F) +
   scale_y_log10() +
+  scale_x_continuous(breaks = c(0, 25, 50, 75, 100), labels = c(0, 25, "", 75, ""), minor_breaks = NULL) + 
   theme_bw() + 
-  labs(y = "Contact rate", x = "Persons present", colour = "Model")
+  labs(y = "Contact rate", x = "Persons present", colour = "Model", linetype = "") + 
+  coord_cartesian(ylim = c(NA, 4))
 
 
 ggsave(plot_N_chosenModels_Slope, filename = paste0("output/Fig2 - rates_over_N_chosenModels_intensitySlope.png")
@@ -120,19 +183,47 @@ for(this_stat in c("PA", "PE")){
     plot_N_chosenModels_Slope_thisthat <- toPlot_N_chosenModels_Slope %>% 
       filter(this_status == this_stat, that_status == that_stat) %>%
       ggplot(aes(x = Nthat)) + 
-      geom_point(aes(y = contactIntensity, colour = factor(".data", levels = names(slope_model_colours))), alpha = 0.3, size = 0.3) + 
+      geom_line(aes(x = Nthat, y = rate_chosenmodel, colour = model_label, linetype = isDiurnal), linewidth = 0.5) +
       # geom_point(aes(x = mean_Nthat, y = obs_intensity, colour = ".data"), alpha = 1) +
       # geom_errorbar(aes(x = mean_Nthat, ymin = obs_intensity - sqrt(var_intensity), ymax = obs_intensity + sqrt(var_intensity), colour = ".data"), alpha = 1) +
-      geom_line(aes(y = rate_chosenmodel, colour = model_label), linewidth = 1.5, show.legend = T) +
-      geom_text(aes(x = Inf, y = 3, label = param_label), parse = T, hjust = 1.0, vjust = 1) +
-      scale_colour_manual(drop = F, values = slope_model_colours) + 
+      # geom_line(aes(y = rate_chosenmodel, colour = model_label), linewidth = 1.0, show.legend = T) +
+      # geom_line(aes(y = rate_chosenmodel, colour = model_label, linetype = diurnality), linewidth = 1.0, show.legend = T) +
+      geom_point(data = ward_intervals_loglik %>% 
+                   filter(this_status == this_stat, that_status == that_stat), 
+                 aes(y = contactIntensity, colour = factor(".data", levels = names(slope_model_colours))), alpha = 0.3, size = 0.3) + 
+      geom_text(data = toPlot_N_chosenModels_Slope %>% 
+                  filter(this_status == this_stat, that_status == that_stat) %>% 
+                  select(newID, Period, param_label) %>% unique, 
+                aes(x = Inf, y = 3, label = param_label), parse = T, hjust = 1.0, vjust = 1) +
+      scale_colour_manual(drop = F, values = slope_model_colours) +
+      scale_linetype_manual(values = c(Diurnal = "solid", `Non-diurnal` = "dashed")) + 
+      # geom_crossbar(aes(x = mean_Nthat, xmin = low_Nthat, xmax = high_Nthat, y = mean_break_rate, colour = "break"), size=1) + 
       facet_nested_wrap(.~newID + Period
-                        , scales = "free_x"
+                        # , scales = "free_x"
                         ,  nrow = 2) +
       # guides(colour = F) +
       scale_y_log10() +
+      scale_x_continuous(breaks = c(0, 25, 50, 75, 100), labels = c(0, 25, "", 75, ""), minor_breaks = NULL) + 
       theme_bw() + 
-      labs(y = "Contact rate", x = "Persons present", colour = "Model")
+      labs(y = "Contact rate", x = "Persons present", colour = "Model", linetype = "")
+    
+    # 
+    # plot_N_chosenModels_Slope_thisthat <- toPlot_N_chosenModels_Slope %>% 
+    #   filter(this_status == this_stat, that_status == that_stat) %>%
+    #   ggplot(aes(x = Nthat)) + 
+    #   # geom_point(aes(x = mean_Nthat, y = obs_intensity, colour = ".data"), alpha = 1) +
+    #   # geom_errorbar(aes(x = mean_Nthat, ymin = obs_intensity - sqrt(var_intensity), ymax = obs_intensity + sqrt(var_intensity), colour = ".data"), alpha = 1) +
+    #   geom_line(aes(y = rate_chosenmodel, colour = model_label), linewidth = 1.0, show.legend = T) +
+    #   geom_point(aes(y = contactIntensity, colour = factor(".data", levels = names(slope_model_colours))), alpha = 0.3, size = 0.3) + 
+    #   geom_text(aes(x = Inf, y = 3, label = param_label), parse = T, hjust = 1.0, vjust = 1) +
+    #   scale_colour_manual(drop = F, values = slope_model_colours) + 
+    #   facet_nested_wrap(.~newID + Period
+    #                     , scales = "free_x"
+    #                     ,  nrow = 2) +
+    #   # guides(colour = F) +
+    #   scale_y_log10() +
+    #   theme_bw() + 
+    #   labs(y = "Contact rate", x = "Persons present", colour = "Model")
     
 
     ggsave(plot_N_chosenModels_Slope_thisthat, filename = paste0("output/SuppFig12 - rates_over_N_chosenModels_intensity_", this_stat, that_stat, ".png")
@@ -157,7 +248,10 @@ grade_plot = ward_nonlinear_plottable %>%
   pivot_wider(id_cols = c("newID", "this_status", "that_status"), names_from = "Period", values_from = c("phi_median", "phi_lo", "phi_hi")) %>%
   left_join(rbind(chosen_model %>% mutate(this_status = "ALL", that_status = "ALL")
                   , chosen_model_thisthat)) %>% 
-  mutate(newID = factor(newID, levels = newID_ref$newID)) %>% 
+  left_join(newID_ref %>% select(newID, newID_hospAbb)) %>% 
+  select(-newID) %>% 
+  rename(newID = newID_hospAbb) %>% 
+  mutate(newID = factor(newID, levels = newID_ref$newID_hospAbb)) %>% 
   mutate(Day_median = case_when(grepl("Constant", ChosenModel) ~ 0,
                                 grepl("Linear", ChosenModel) ~ 1,
                                 ChosenModel == "Non-linear diurnal" ~ phi_median_Day,
@@ -202,7 +296,10 @@ grade_plot = ward_nonlinear_plottable %>%
 grade_catHosp_plot = ward_nonlinear_catHosp_plottable %>% 
   pivot_wider(id_cols = c("newID", "this_catHosp", "that_catHosp"), names_from = "Period", values_from = c("phi_median", "phi_lo", "phi_hi")) %>%
   left_join(chosen_model_catHosp_allthisthat) %>% 
-  mutate(newID = factor(newID, levels = newID_ref$newID)) %>% 
+  left_join(newID_ref %>% select(newID, newID_hospAbb)) %>% 
+  select(-newID) %>% 
+  rename(newID = newID_hospAbb) %>% 
+  mutate(newID = factor(newID, levels = newID_ref$newID_hospAbb)) %>% 
   mutate(Day_median = case_when(grepl("Constant", ChosenModel) ~ 0,
                                 grepl("Linear", ChosenModel) ~ 1,
                                 ChosenModel == "Non-linear diurnal" ~ phi_median_Day,
@@ -280,6 +377,8 @@ ggsave(paste0("output/Fig1 - phi_daynight_ALL_toPAchosen.pdf"), width = 30, heig
 
 
 grade_catHosp_plot %>% 
+  filter(!is.na(median)) %>% 
+  
   mutate(thiscH_label = case_when(this_catHosp == "nurse" ~ "Nurse"
                                   , this_catHosp == "aux nurse" ~ "Auxiliary nurse"
                                   , this_catHosp == "physician" ~ "Physician"
@@ -298,18 +397,18 @@ grade_catHosp_plot %>%
                                               "Physician>Patients", 
                                               "Administration>Patients"))) %>% 
   ggplot(aes(y = newID)) + 
+  geom_vline(aes(xintercept = 0), linetype = "dotted")+ 
+  geom_vline(aes(xintercept = 1), linetype = "dashed") + 
   geom_point(aes(x = median, colour = colour, shape = isDiurnal)) + 
   geom_errorbarh(aes(xmin = lo, xmax = hi, colour = colour), height=error_bar_width) +
   facet_nested(.~thisthat_label + Period) + 
   theme_bw() + 
   labs(y = "", x = expression(phi), colour = "Best fit model", shape = "") + 
   scale_y_discrete(limits=rev) + 
+  theme(axis.text.x = element_text(size = 7)) + 
   scale_colour_manual(values = model_cols) +
   scale_shape_manual(values = c(`Diurnal model` = "circle", `Non-diurnal model` = "square")) + 
-  geom_vline(aes(xintercept = 0), linetype = "dotted")+ 
-  geom_vline(aes(xintercept = 1), linetype = "dashed") + 
   geom_hline(aes(yintercept = 1.5), linetype = "solid")
-
 
 ggsave(paste0("output/Fig3 - phi_daynight_catHosp.png"), width = 25, height = 12, units = "cm")
 ggsave(paste0("output/Fig3 - phi_daynight_catHosp.pdf"), width = 25, height = 12, units = "cm", device = "pdf")
@@ -347,7 +446,8 @@ grade_plot %>%
   ggplot() + 
   # geom_vline(aes(xintercept = 0), linetype = "solid")+ 
   geom_bar(aes(y = newID, x = increase_110, fill = colour), stat = "identity") + 
-  
+  geom_errorbarh(aes(y = newID, xmin = increase_110_lo, xmax = increase_110_hi), colour = "black", height = 0.5) +
+  geom_errorbarh(aes(y = newID, xmin = increase_110_lo, xmax = increase_110_hi, colour = colour), alpha = 0.8, height = 0.5) +
   # coord_flip() + 
   facet_nested(.~thisthat_label + Period) + 
   theme_bw() + 
